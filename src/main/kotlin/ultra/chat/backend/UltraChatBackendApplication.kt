010dca7b45ae619fd0e54806c613a3d05c8599dc
AndriyBosik <andriybosik2002@ukr.net>
2021-10-06 13:47:25 +0600

    Startup batch file was added

--- src/main/kotlin/ultra/chat/backend/UltraChatBackendApplication.kt
+++ src/main/kotlin/ultra/chat/backend/UltraChatBackendApplication.kt
@@ -4,8 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class UltraChatBackendApplication
+class UltraChatBackendApplication {
 
-fun main(args: Array<String>) {
-    runApplication<UltraChatBackendApplication>(*args)
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<UltraChatBackendApplication>(*args)
+        }
+    }
 }
